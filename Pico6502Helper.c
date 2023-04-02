9cdf8ceaa50cecd52f53e631a2ab911d5a126b70
Phil <PhilboBaggins@users.noreply.github.com>
2023-04-03 01:12:08 +1000

    Cleanup

--- Pico6502Helper.c
+++ Pico6502Helper.c
@@ -4,22 +4,18 @@
 #include "hardware/timer.h"
 #include "hardware/clocks.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
+int64_t alarm_callback(alarm_id_t id, void *user_data)
+{
     // Put your timeout handler code in here
     return 0;
 }
-
-
 
 int main()
 {
     stdio_init_all();
 
-
     // Timer example code - This example fires off the callback after 2000ms
     add_alarm_in_ms(2000, alarm_callback, NULL, false);
-
-
 
     puts("Hello, world!");
 
